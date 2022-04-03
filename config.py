8b0ff2c7b8158b5413039011d7f849486c067cdd
MeKLiN2 <48744549+MeKLiN2@users.noreply.github.com>
2022-04-03 14:49:37 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,106 @@
+# Tinychat account.
+ACCOUNT = 'TinyChatAccount'
+
+# Password for account
+PASSWORD = 'YourPassword'
+
+# Fallback rtc version.
+FALLBACK_RTC_VERSION = '2.0.22-4'
+
+# Log chat messages and events.
+CHAT_LOGGING = True
+
+# Show additional info/errors in console.
+DEBUG_MODE = False
+
+# Log debug info to file.
+DEBUG_TO_FILE = False
+
+# Logging level for the debug file.
+DEBUG_LEVEL = 30
+
+# Use colors for the console.
+CONSOLE_COLORS = True
+
+# Time format.
+USE_24HOUR = False
+
+# The name of pinylib's debug log file.
+DEBUG_FILE_NAME = 'pinylib_debug.log'
+
+# The path to the config folder.
+CONFIG_PATH = 'rooms/'
+
+# bot
+# The prefix used for bot commands.
+B_PREFIX = '!'
+
+# Bot controller key.
+#B_KEY = 'jimdh9189dh91298d2'
+
+# Bot super key.
+#B_SUPER_KEY = 'jim89932h893f89fjk6k'
+
+# The maximum amount of match bans.
+B_MAX_MATCH_BANS = 1
+
+# Public commands enabled.
+B_PUBLIC_CMD = False
+
+# public skip
+B_PUBLIC_SKIP = True
+
+# Greet user when joining.
+B_GREET = False
+
+# Allow guests to enter the room. Allows anyone without an account to come in
+B_ALLOW_GUESTS = True
+
+# Allow lurkers to enter the room. Lurkers are those who dont solve captcha, usually spammers.
+B_ALLOW_LURKERS = True
+
+# Allow guest nicks. "guest-000", usually spammers.
+B_ALLOW_GUESTS_NICKS = True
+
+#Allow 200 Character + spam
+B_AllowSpam = True
+
+# Use kick instead of ban for auto banning.
+B_USE_KICK_AS_AUTOBAN = False
+
+# The file name of nick bans.
+B_NICK_BANS_FILE_NAME = 'nick_bans.txt'
+
+# A list of all the nick bans.
+B_NICK_BANS = []
+
+# The file name of account bans.
+B_ACCOUNT_BANS_FILE_NAME = 'account_bans.txt'
+
+# A list of account bans.
+B_ACCOUNT_BANS = []
+
+# The file name of string(words) bans.
+B_STRING_BANS_FILE_NAME = 'string_bans.txt'
+
+# A list of string bans.
+B_STRING_BANS = []
+
+# The name of the bot's debug file.
+B_DEBUG_FILE_NAME = 'tinybot_debug.log'
+
+# youtube plays
+B_Youtube_Play_List = 'youtube_logs.txt'
+
+# youtube filter strings
+B_Youtube_Ban_List = 'youtube_filter.txt'
+
+#Admin List
+B_Admins = 'Admins.txt'
+
+B_known = 'Known.txt'
+
+B_vip = False
+
+# Check if VIP and message chat.
+B_CheckUser = False
